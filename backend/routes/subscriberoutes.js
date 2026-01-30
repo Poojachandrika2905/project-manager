2c9c742cb8993835f77f82a35c1c6e0f2ef3d341
Poojachandrika2905 <pooja.chandrika2004@gmail.com>
2026-01-30 13:46:33 +0530

    Fix Subscriber import path (models folder)

--- backend/routes/subscriberoutes.js
+++ backend/routes/subscriberoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Subscriber = require("../model/Subscriber");
+const Subscriber = require("../models/Subscriber");
 
 // ADD subscriber
 router.post("/", async (req, res) => {
